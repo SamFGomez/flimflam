e2a547525753d6a2e57a30e40549f3e6d8a436ce
Sam Gomez <samg@macbook-pro.mynetworksettings.com>
2025-12-07 18:52:18 -0500

    ToDo Stuff

--- src/components/ToDo.js
+++ src/components/ToDo.js
@@ -2,17 +2,44 @@ import { Component } from 'react';
 
 
 class ToDo extends Component {
+
+  constructor(props){
+      super(props);
+      this.state = {
+          messages: []
+      }
+  }
+
+  addNewToDOItem(message){
+    var {messages} = this.state;
+    messages.push(message);
+    this.setState({messages: messages});
+  }
+
+  componentDidMount(){
+    document.addEventListener('keydown', (e)=>{
+      if(e.key === 'Enter') this.addNewToDOItem(e.target.value);
+    });
+  }
+
   render() {
+    var {messages} = this.state;
+
     return (
       <div class="defaultContainer">
         <h1> ToDo List</h1>
         <div> 
-          <p> 1. Implement ToDo functionality </p>
-          <p> 2. Create Other Components </p>
-          <p> 3. Display daily weather </p>
-          <p> 4. Create Web API (raspberry pi?) </p>
-          <p> 5. Create DB (raspberry pi?)</p>
-
+          {messages.map((x,i)=>{
+            return (
+              <div class="bulletContainer">
+                <p>{`${messages.indexOf(x)+1}. ${x}`}</p>
+              </div>
+            );
+          })}
+          <input id="newToDoInput" type="text" placeholder="New ToDo Item" />
+          <button style={{display:"none"}} type="submit" onClick={()=>this.addNewToDOItem(document.getElementById("newToDoInput").value)}> 
+            Add ToDo Item 
+          </button>
         </div>
       </div>
     );
